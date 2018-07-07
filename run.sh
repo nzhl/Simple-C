2a05f7f2002f875296c6949fa56be4e39ba6c5e4
ZHIMIN ZHANG <psyzz2@nottingham.ac.uk>
2018-07-07 23:35:01 +0800

    cleaned it

--- run.sh
+++
@@ -1,8 +0,0 @@
-# !/bin/basA
-
-rm -rf */*.class
-rm -rf */*/*.class
-rm -rf */*/*/*.class
-
-javac virtualMachine/Interpreter.java
-java virtualMachine/Interpreter
--- src/virtualMachine/Config.java
+++ src/virtualMachine/Config.java
@@ -5,6 +5,6 @@ public class Config {
      *  the path of your source code and the output code , please use absolute path.
      */
 
-    public final static String SRC = "/Users/zhangzhimin/test.c";
-    public final static String OUT = "/Users/zhangzhimin/out.txt";
+    public final static String SRC = "./test.c";
+    public final static String OUT = "./out.txt";
 }
